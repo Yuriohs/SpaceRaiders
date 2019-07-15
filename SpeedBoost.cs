39a5f3711966c84b35bbf11f8bff61a3a880218d
Davon Sanders <gmzcharlotte@gmail.com>
2019-07-14 23:54:20 -0400

    Add files via upload

---
+++ SpeedBoost.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBoost : MonoBehaviour {
+
+    public GameObject boost;
+    public GameObject player; //placeholdder, since PlayerScript seems to determine what the player is, can't reference player gameobject(I think)
+    bool isboosting = false;
+    [SerializeField]
+    private float baseSpeed = 5;
+    private float maxSpeed; // will possibly be used as a speed cap
+    private float speedAddition;
+    private float playerSpeed;
+
+
+    public void gainSpeed()
+    {
+        if (isboosting)
+            playerSpeed = baseSpeed + speedAddition;
+        else
+            playerSpeed = baseSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        speedAddition = 5; // example speed boost valaue
+        isboosting = true;
+        if (other.tag == "Booster")
+        {
+            playerSpeed = baseSpeed + speedAddition;
+        }
+    }
+}
+
